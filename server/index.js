4f0cd3721a10c0b0d047c8d7004d9728ac47dc31
avishaar <ashaar928@gmail.com>
2023-04-18 23:59:54 +0200

    connect server to client

--- server/index.js
+++ server/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 app.use(cors());
+
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello Avigail');
-  });
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+});
 
 server.listen(4000, () => 'Server is running on port 4000');
