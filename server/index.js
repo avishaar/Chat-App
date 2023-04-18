643b005536698712b0336e4a563ee2d6c602f36d
avishaar <ashaar928@gmail.com>
2023-04-19 00:08:42 +0200

    join user to room and send message to room members

--- server/index.js
+++ server/index.js
@@ -15,8 +15,38 @@ const io = new Server(server, {
   },
 });
 
+const CHAT_BOT = 'ChatBot';
+
+let chatRoom = '';
+let allUsers = [];
+
 io.on('connection', (socket) => {
   console.log(`User connected ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    const { username, room } = data;
+    socket.join(room);
+
+    let __createdtime__ = Date.now();
+    
+    socket.to(room).emit('receive_message', {
+      message: `${username} has joined the chat room`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+    socket.emit('receive_message', {
+      message: `Welcome ${username}`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+    
+  chatRoom = room;
+  allUsers.push({ id: socket.id, username, room });
+  chatRoomUsers = allUsers.filter((user) => user.room === room);
+  socket.to(room).emit('chatroom_users', chatRoomUsers);
+  socket.emit('chatroom_users', chatRoomUsers);
+  });
 });
 
 server.listen(4000, () => 'Server is running on port 4000');
