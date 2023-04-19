dfec9b0b55afde94bb239ae3c9fb3355947d52b0
avishaar <ashaar928@gmail.com>
2023-04-19 23:15:02 +0200

    more styling

--- client/src/pages/chat/index.js
+++ client/src/pages/chat/index.js
@@ -2,6 +2,7 @@ import styles from './styles.module.css';
 import RoomAndUsersColumn from './room-and-users';
 import MessagesReceived from './messages';
 import SendMessage from './send-message';
+import Icon from '../img/tti-logo.png';
 
 const Chat = ({ username, room, socket }) => {
   return (
@@ -10,6 +11,9 @@ const Chat = ({ username, room, socket }) => {
       <div>
         <MessagesReceived socket={socket} />
         <SendMessage socket={socket} username={username} room={room} />
+      </div>
+      <div>
+        <img src={Icon} alt="" />
       </div>
     </div>
   );
